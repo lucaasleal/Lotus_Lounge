9dc0bb12dcf1898120aef5fe387f15a5dc70f637
mbcv <matheusvieira2006@gmail.com>
2025-04-08 21:51:07 -0300

    Add Tela Inicial

--- game.c
+++ game.c
@@ -4,27 +4,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-void text_board(int *framesCounter, int *letterCount, int *velocidade){
+bool text_board(int *framesCounter, int *letterCount, int *velocidade, const char *text[], int numLines) {
     BeginDrawing();
-    const char text[] = "Andre eh um corninho!";
+    ClearBackground(BLACK);
 
-    ClearBackground(RAYWHITE);
-    Texture2D texture = LoadTexture("imagens\\text_board.png");
+    Texture2D texture = LoadTexture("assets/imagens/text_board.png");
+    Font fonte = LoadFont("assets/fonts/Pixelate-Regular.ttf");
 
-    // Pegando o tamanho da tela
     int screenWidth = GetScreenWidth();
     int screenHeight = GetScreenHeight();
 
-    // Definindo as áreas de origem e destino
-    Rectangle sourceRec = { 0, 0, texture.width, texture.height }; // Toda a imagem
-    Rectangle destRec = { 0, 0, screenHeight, screenHeight}; // Ocupa a tela inteira
+    Rectangle sourceRec = { 0, 0, texture.width, texture.height };
+    Rectangle destRec = { 0, 0, screenHeight, screenHeight };
     Vector2 origin = { 0, 0 };
-    Font fonte = LoadFont(".//assets//fonts//Pixelate-Regular.ttf");
+
     DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE);
+
     (*framesCounter)++;
-    if (*framesCounter % *velocidade == 0 && *letterCount < (int)strlen(text)) {
-        (*letterCount)++;
+    if (*framesCounter % *velocidade == 0) (*letterCount)++;
+
+    int totalLetters = 0;
+    for (int i = 0; i < numLines; i++) {
+        totalLetters += strlen(text[i]);
     }
-    DrawTextEx(fonte, TextSubtext(text, 0, *letterCount), (Vector2){screenWidth / 10, screenHeight / 2}, 40, 2, LIGHTGRAY);
+
+    int lettersDrawn = 0;
+    int yOffset = 0;
+    int fontSize = 30;
+    int lineSpacing = 40;
+    int startY = screenHeight / 4;
+
+    for (int i = 0; i < numLines; i++) {
+        int len = strlen(text[i]);
+        if (*letterCount >= lettersDrawn + len) {
+            DrawTextEx(fonte, text[i], (Vector2){screenWidth / 10, startY + yOffset}, fontSize, 2, LIGHTGRAY);
+        } else {
+            char buffer[1024] = {0};
+            strncpy(buffer, text[i], *letterCount - lettersDrawn);
+            DrawTextEx(fonte, buffer, (Vector2){screenWidth / 10, startY + yOffset}, fontSize, 2, LIGHTGRAY);
+            break;
+        }
+        lettersDrawn += len;
+        yOffset += lineSpacing;
+    }
+
     EndDrawing();
+
+    return *letterCount >= totalLetters + 10;  // +20 só pra garantir que dê tempo de ver o final
 }
